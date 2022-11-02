b6ec8c240b79223ff4c14fe0a384622b3c3a936c
Xie-Zong-Lin <79505070+Xie-Zong-Lin@users.noreply.github.com>
2022-11-02 19:08:03 +0800

    Add files via upload

---
+++ LAB05/script.js
@@ -0,0 +1,44 @@
+
+function changeWidth() {
+  document.getElementById('myTable').width="200";
+}
+
+function changeWidth2() {
+  document.getElementById('myTable').width="500";
+}
+
+function changeBorder() {
+  document.getElementById('myTable').border="1px";
+}
+
+function changeBorder2() {
+  document.getElementById('myTable').border="10";
+}
+
+function changeBorder3() {
+  document.getElementById('myTable').border="20";
+}
+
+function changeColor() {
+  document.getElementById('first').bgColor="#7CEEA6";
+  document.getElementById('second').bgColor="#7CEEA6";
+}
+function changeColor2() {
+  document.getElementById('first').bgColor="#7ED4F9";
+  document.getElementById('second').bgColor="#7ED4F9";
+}
+function changeColor3() {
+  document.getElementById('first').bgColor="#AC8EAB";
+  document.getElementById('second').bgColor="#AC8EAB";
+}
+function changeColor4() {
+  document.getElementById('first').bgColor="#FFE4B5";
+  document.getElementById('second').bgColor="#FFE4B5";
+}
+
+function Reset(){
+  document.getElementById('myTable').border="1";
+  document.getElementById('myTable').width="150";
+  document.getElementById('first').bgColor="#E8F953";
+  document.getElementById('second').bgColor="#E8F953";
+}
